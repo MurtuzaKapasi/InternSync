02b56affebe2f705315ba3cffff8f2cf17c0302a
Asymtode712 <siddheyakul@gmail.com>
2024-03-16 20:43:59 +0530

    Router Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Homepage from './pages/homepage/Homepage';
+import Loginpage from './pages/loginpage/Loginpage'
 import Intern from './pages/dashboard/Intern/Intern';
-import { BrowserRouter ,Route, Routes} from 'react-router-dom';
+import Register from './pages/register/Register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RecruiterCard from './pages/dashboard/Intern/RecruiterCard';
 function App() {
 
@@ -14,12 +16,11 @@ function App() {
         <Route path='/intern' element={<Intern />} />
         <Route path="/intern/:id" element={<RecruiterCard />} />
         <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Loginpage />} />
-        <Route path='*' element={<Homepage />} />
+        <Route path="/login" element={<Loginpage />} />
 
       </Routes>
     </BrowserRouter>
-    
+
   )
 }
 
